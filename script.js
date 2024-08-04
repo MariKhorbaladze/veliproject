39dad7d1421727a074872efa7a638a71618f2cb4
MariKhorbaladze <129887035+MariKhorbaladze@users.noreply.github.com>
2024-08-04 19:15:24 +0400

    add js

--- script.js
+++ script.js
@@ -11,3 +11,31 @@ document.getElementById('languageToggle').addEventListener('change', function() 
         switcherText.textContent = 'Hello';
     }
 });
+
+// const searchButton = document.getElementById('searchButton');
+
+// searchButton.addEventListener('click', () => {
+   
+// });
+
+
+
+//  product sections 
+
+function toggleDropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn') && !event.target.matches('.category-item')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+
